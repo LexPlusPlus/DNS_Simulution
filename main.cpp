ef6bddbe82fbccfc69e96220dd51d049ec223900
Lex <lexverheem@gmail.com>
2021-12-08 21:37:34 +0100

    simple ddiffusion matrix builder

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <Eigen/Sparse>
+#include <Eigen/Dense>
 #include <Eigen/IterativeLinearSolvers>
+#include <Eigen/Sparse> 
+
 #include <omp.h>
 
 void createMatrixEntries(Eigen::VectorXd &b, Eigen::SparseMatrix<double> &A, std::vector<Eigen::Triplet<double>> &tripletList);
 
 int main(){
     
-    int n = 10;
+    int n = 5;
     Eigen::VectorXd b(n), x(n);
     Eigen::SparseMatrix<double> A(n,n);
     std::vector<Eigen::Triplet<double>> tripletList(3*n - 2);
@@ -26,31 +29,31 @@ int main(){
 void createMatrixEntries(Eigen::VectorXd &b, Eigen::SparseMatrix<double> &A, std::vector<Eigen::Triplet<double>> &tripletList){
     // Γ = Gamma, Φ = Phi, ρ = rho.
     // D_i = (Γ/δx)_i, F_i = (ρu)_i
-    double α = -1;
-    double β =  1;
-    double γ =  1;
+    double alpha = -1;
+    double beta =  1;
+    double gamma =  1;
     int i = 0;
     
     for(int row = 0; row < A.rows(); row++){
 
         for(int col = 0; col < A.cols(); col++){
-            
+                        
             if(row -  1 == col){ // lower diagonal
-                tripletList[i] = Eigen::Triplet<double>(row,col,α);
+                tripletList[i] = Eigen::Triplet<double>(row,col,alpha);
                 i++;
             }
             if(row == col){ // main diagonal
-                tripletList[i] = Eigen::Triplet<double>(row,col,β);
+                tripletList[i] = Eigen::Triplet<double>(row,col,beta);
                 i++;
             }
             if(row + 1 == col){ // upper diagonal
-                tripletList[i] = Eigen::Triplet<double>(row,col,γ);
+                tripletList[i] = Eigen::Triplet<double>(row,col,gamma);
                 i++;
             }
         }
     }
-    b(0) = -α;
-    b(A.cols()) = 3; 
+    b(0) = -alpha;
+    // b(A.cols() - 1) = 3; 
     A.setFromTriplets(tripletList.begin(), tripletList.end());
 }
 
